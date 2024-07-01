66bf7302ec2415d88e4c83b5641638320a94cc92
jabber <2898117012@qq.com>
2024-07-01 19:59:43 +0800

    fix: alipay

--- WOO_AliPay_HK_Gateway.php
+++ WOO_AliPay_HK_Gateway.php
@@ -1,5 +1,8 @@
 <?php
 
+use Shopeo\WooAlipayHK\Alipay\SignatureTool;
+use Shopeo\WooAlipayHK\AliPayApi;
+
 class WOO_AliPay_HK_Gateway extends \WC_Payment_Gateway {
 	private $sandbox = false;
 	private $client_id = '';
@@ -72,10 +75,43 @@ class WOO_AliPay_HK_Gateway extends \WC_Payment_Gateway {
 
 	public function process_payment( $order_id ) {
 		global $woocommerce;
+		$order       = new WC_Order( $order_id );
+		$alipayApi   = new AliPayApi( $this->client_id, $this->public_key, $this->private_key, $this->sandbox );
+		$notifyUrl   = home_url() . '/wc-api/' . $this->id . '?id=' . $order_id;
+		$redirectUrl = $this->get_return_url( $order );
+		$result      = $alipayApi->newOrder( $order_id, $order->get_currency(), $order->get_total(), $notifyUrl, $redirectUrl );
+		if ( $result ) {
+			error_log( print_r( $result, true ) );
+			$order->set_transaction_id( $result->paymentId );
+			$order->save();
+			$woocommerce->cart->empty_cart();
+
+			return array( 'result' => 'success', 'redirect' => $result->normalUrl );
+		}
 	}
 
 
 	public function webhook() {
-
+		$order = new WC_Order( $_GET['id'] );
+		error_log( 'webhook' );
+		$res  = file_get_contents( "php://input" );
+		$body = json_decode( $res, true );
+		error_log( print_r( $body, true ) );
+		$headers  = getallheaders();
+		$resTime  = $headers['request-time'];
+		$clientId = $headers['client-id'];
+		if ( $body['result']['resultCode'] == 'SUCCESS' && $order->get_transaction_id() == $body['paymentId'] ) {
+			$order->payment_complete();
+			wc_reduce_stock_levels( $order->get_id() );
+			header( 'response-time', $resTime );
+			header( 'client-id', $clientId );
+			wp_send_json( [
+				'result' => [
+					'resultCode'    => 'SUCCESS',
+					'resultStatus'  => 'S',
+					'resultMessage' => 'Success'
+				]
+			] );
+		}
 	}
 }
--- src/AliPayApi.php
+++ src/AliPayApi.php
@@ -2,28 +2,64 @@
 
 namespace Shopeo\WooAlipayHK;
 
+use Shopeo\WooAlipayHK\Alipay\DefaultAlipayClient;
+use Shopeo\WooAlipayHK\Alipay\Model\Amount;
+use Shopeo\WooAlipayHK\Alipay\Model\CustomerBelongsTo;
+use Shopeo\WooAlipayHK\Alipay\Model\Env;
+use Shopeo\WooAlipayHK\Alipay\Model\Order;
+use Shopeo\WooAlipayHK\Alipay\Model\PaymentMethod;
+use Shopeo\WooAlipayHK\Alipay\Model\ProductCodeType;
+use Shopeo\WooAlipayHK\Alipay\Model\TerminalType;
+use Shopeo\WooAlipayHK\Alipay\Request\Pay\AlipayPayRequest;
+
 class AliPayApi {
-	private $host = 'https://open-sea-global.alipay.com/ams/';
+	private $host = 'https://open-sea-global.alipay.com/';
 	private $clientId = '';
 	private $publicKey = '';
 	private $privateKey = '';
+	private $sandbox = true;
 
 	public function __construct( $clientId, $publicKey, $privateKey, $sandbox = true ) {
 		$this->clientId   = $clientId;
 		$this->publicKey  = $publicKey;
 		$this->privateKey = $privateKey;
-		if ( $sandbox ) {
-			$this->host .= 'sandbox/api/v1/';
-		} else {
-			$this->host .= 'api/v1/';
+		$this->sandbox    = $sandbox;
+	}
+
+	public function newOrder( $order_id, $currency, $amount, $notify_url, $redirect_url ) {
+		$alipayPayRequest = new AlipayPayRequest();
+		$path             = $this->sandbox ? '/ams/sandbox/api/v1/payments/pay' : '/ams/api/v1/payments/pay';
+
+		$paymentMethod = new PaymentMethod();
+		$paymentMethod->setPaymentMethodType( CustomerBelongsTo::ALIPAY_HK );
+		$alipayPayRequest->setPaymentMethod( $paymentMethod );
+
+		$order = new Order();
+		$order->setOrderDescription( 'Online buy' );
+		$order->setReferenceOrderId( $order_id );
+
+		$orderAmount = new Amount();
+		$orderAmount->setCurrency( $currency );
+		$orderAmount->setValue( floatval( $amount ) * 100 );
+		$order->setOrderAmount( $orderAmount );
+
+		$env = new Env();
+		$env->setTerminalType( TerminalType::WEB );
+		$order->setEnv( $env );
+
+		$alipayPayRequest->setClientId( $this->clientId );
+		$alipayPayRequest->setPath( $path );
+		$alipayPayRequest->setProductCode( ProductCodeType::CASHIER_PAYMENT );
+		$alipayPayRequest->setPaymentRequestId( $order_id );
+		$alipayPayRequest->setPaymentAmount( $orderAmount );
+		$alipayPayRequest->setOrder( $order );
+		$alipayPayRequest->setPaymentNotifyUrl( $notify_url );
+		$alipayPayRequest->setPaymentRedirectUrl( $redirect_url );
+		$alipayClient = new DefaultAlipayClient( $this->host, $this->privateKey, $this->publicKey );
+		try {
+			return $alipayClient->execute( $alipayPayRequest );
+		} catch ( \Exception $exception ) {
+			error_log( 'Alipay Error!' );
 		}
-	}
-
-	private function signature() {
-
-	}
-
-	private function requestTime() {
-		return round( microtime( true ) * 1000 );
 	}
 }
